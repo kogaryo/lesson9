d211626eebb0ac5395aad33f3d39489880cc487a
kogaryo <g1727025@st.t-kougei.ac.jp>
2019-11-13 18:02:44 +0900

    プロジェクト一緒

---
+++ 1_trail_renderer/New Unity Project (14)/Assets/PlayerCOntroller.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCOntroller : MonoBehaviour
+{
+    public float spead = 3.0f;
+    GameObject sword;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sword = transform.GetChild(0).gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(Input.GetKey("up"))
+
+        //if (Input.GetKey("down"))
+
+        Swipe();
+    }
+
+    void Swipe()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 target_pos = ray.GetPoint(5.0f);
+
+            sword.transform.LookAt(target_pos);
+        }
+    }
+}
